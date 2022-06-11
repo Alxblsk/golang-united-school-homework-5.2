78cfdc611122e6d2ff84e3a41ec6155f035f72bb
Aliaksei Belski <aliaksei.belski@epicgames.com>
2022-06-11 16:14:49 -0700

    Do cleanup correctly

--- cache.go
+++ cache.go
@@ -1,44 +1,65 @@
-package cache
+package main
 
 import (
 	"time"
 )
 
 type Cache struct {
-	data map[string]string
+	expirations map[string]time.Time
+	data        map[string]string
 }
 
 func NewCache() Cache {
 	return Cache{
-		data: map[string]string{},
+		expirations: map[string]time.Time{},
+		data:        map[string]string{},
 	}
 }
 
 func (c *Cache) Get(key string) (string, bool) {
+	c.cleanup()
+
 	val, ok := c.data[key]
 	return val, ok
 }
 
 func (c *Cache) Put(key string, value string) {
+	c.cleanup()
+
 	c.data[key] = value
 }
 
 func (c *Cache) Keys() []string {
+	c.cleanup()
+
 	keysList := make([]string, len(c.data))
 
-	for _, val := range c.data {
-		keysList = append(keysList, val)
+	for key, _ := range c.data {
+		keysList = append(keysList, key)
 	}
 
 	return keysList
 }
 
 func (c *Cache) PutTill(key string, value string, deadline time.Time) {
-	c.data[key] = value
+	c.cleanup()
+	c.Put(key, value)
 
-	timeDiff := deadline.Sub(time.Now())
+	c.expirations[key] = deadline
+}
 
-	// Should be a cleanup before every other operation instead; but let's start with a sync method
-	time.Sleep(timeDiff)
-	delete(c.data, key)
+func (c *Cache) cleanup() {
+	for key, _ := range c.expirations {
+		c.removeExpired(key)
+	}
+}
+
+func (c *Cache) removeExpired(key string) {
+	if deadline, ok := c.expirations[key]; ok {
+		timeDiff := deadline.Sub(time.Now())
+		if timeDiff <= 0 {
+			delete(c.data, key)
+			delete(c.expirations, key)
+		}
+	}
 }
