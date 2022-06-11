df2d11b16cdd0331104d0f6d883d1bf4e0867be8
Aliaksei Belski <aliaksei.belski@epicgames.com>
2022-06-11 13:04:40 -0700

    Initial Cache struct

--- cache.go
+++ cache.go
@@ -1,23 +1,44 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	data map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		data: map[string]string{"xyz": "yes"},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	val, ok := c.data[key]
+	return val, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key string, value string) {
+	c.data[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keysList := make([]string, len(c.data))
+
+	for _, val := range c.data {
+		keysList = append(keysList, val)
+	}
+
+	return keysList
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key string, value string, deadline time.Time) {
+	c.data[key] = value
+
+	timeDiff := deadline.Sub(time.Now())
+
+	// Should be a cleanup before every other operation instead; but let's start with a sync method
+	time.Sleep(timeDiff)
+	delete(c.data, key)
 }
